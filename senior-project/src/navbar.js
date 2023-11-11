43577a072a1c40d44b3abecf67254d741b7705aa
Anna-Mclenko <amikhailenko@ewu.edu>
2023-11-10 20:12:39 -0800

    minor reorganization in Navbar

--- senior-project/src/navbar.js
+++ senior-project/src/navbar.js
@@ -2,22 +2,22 @@ import Home from "./home";
 import Map from "./map";
 import Calendar from "./calendar";
 import Profile from "./profile";
-import { Routes, Route, Navigate, BrowserRouter, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function Navbar () {
     return (
     <>       
-        <Routes>
-            <Route path="/home" element={<Home/>}/>
-            <Route path="/mapview" element={<Map/>}/>
-            <Route path="calendarview" element={<Calendar/>}/>
-            <Route path="profile" element={<Profile/>}/>
-        </Routes>
-
     <Link to="/home">Home</Link>
     <Link to="/mapview">Map</Link>
     <Link to="/calendarview">Cal</Link>
     <Link to="/profile">Profile</Link>
+    <Routes>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/mapview" element={<Map/>}/>
+        <Route path="calendarview" element={<Calendar/>}/>
+        <Route path="profile" element={<Profile/>}/>
+    </Routes>
+
     </>
     );
 }
