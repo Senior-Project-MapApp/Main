17b07d86f7cd26a1cde61b6b53fd971cba914538
Anna-Mclenko <amikhailenko@ewu.edu>
2023-11-17 15:41:01 -0800

    added the material ui lib for stylingredownloaded

--- senior-project/src/navbar.js
+++ senior-project/src/navbar.js
@@ -3,14 +3,23 @@ import Map from "./map";
 import Calendar from "./calendar";
 import Profile from "./profile";
 import { Routes, Route, Link } from 'react-router-dom';
+import { AppBar, IconButton, Toolbar } from "@mui/material";
 
 function Navbar () {
     return (
     <>       
-    <Link to="/home">Home</Link>
-    <Link to="/mapview">Map</Link>
-    <Link to="/calendarview">Cal</Link>
-    <Link to="/profile">Profile</Link>
+    <AppBar position="static">
+        <Toolbar>
+            <IconButton size="large" edge="start" color="green">
+
+            </IconButton>
+        </Toolbar>
+        <Link to="/home">Home</Link>
+        <Link to="/mapview">Map</Link>
+        <Link to="/calendarview">Cal</Link>
+        <Link to="/profile">Profile</Link>
+
+    </AppBar>
     <Routes>
         <Route path="/home" element={<Home/>}/>
         <Route path="/mapview" element={<Map/>}/>
