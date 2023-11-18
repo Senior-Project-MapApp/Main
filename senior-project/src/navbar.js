1e907d9659c7daac293717b3dc62a12299c84392
Anna-Mclenko <amikhailenko@ewu.edu>
2023-11-17 17:25:01 -0800

    added a Navigation bar

--- senior-project/src/navbar.js
+++ senior-project/src/navbar.js
@@ -3,23 +3,27 @@ import Map from "./map";
 import Calendar from "./calendar";
 import Profile from "./profile";
 import { Routes, Route, Link } from 'react-router-dom';
-import { AppBar, IconButton, Toolbar } from "@mui/material";
+import { AppBar, IconButton, Toolbar, Box, Grid } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Navbar () {
     return (
-    <>       
-    <AppBar position="static">
-        <Toolbar>
-            <IconButton size="large" edge="start" color="green">
-
-            </IconButton>
-        </Toolbar>
-        <Link to="/home">Home</Link>
-        <Link to="/mapview">Map</Link>
-        <Link to="/calendarview">Cal</Link>
-        <Link to="/profile">Profile</Link>
-
-    </AppBar>
+    <>      
+    <Box sx={{flexGrow: 1}}>
+        <AppBar position="static">
+        <Grid container direction={"row"} justifyContent={"flex-start"} alignItems={"center"} >
+            <Toolbar>
+                <IconButton size="large" edge="start" color="green" sx={{mr: 2}}>
+                    <MenuIcon/>
+                </IconButton>
+            </Toolbar>
+            <Link hrefLang="/home" underline="hover" >Home</Link>
+            <Link to="/mapview">Map</Link>
+            <Link to="/calendarview">Cal</Link>
+            <Link to="/profile">Profile</Link>
+            </Grid>
+        </AppBar>
+    </Box>
     <Routes>
         <Route path="/home" element={<Home/>}/>
         <Route path="/mapview" element={<Map/>}/>
