e8e40cedd99b5294ed60766507c4ef92d06338cf
Anna-Mclenko <amikhailenko@ewu.edu>
2024-01-11 23:09:47 -0800

    added some new info into the profile page

--- senior-project/src/profile.js
+++ senior-project/src/profile.js
@@ -9,6 +9,10 @@ function Profile ({data}) {
             <Box sx={{ margin: "2%", marginTop: "5%"}}>
                 <Avatar variant="rounded" sx={{bgcolor: green[900], width: 100, height: 100}}></Avatar>
                 <Typography variant="h4">{data.userName}</Typography>
+                <Typography>{data.joined}</Typography>
+                <Typography>Number of Locations: {data.numloc}</Typography>
+                <Typography>Total Tasks: {data.numTasks}</Typography>
+                <Typography>Done Tasks: {data.doneTasks}</Typography>
             </Box>
                 <Box>
                 </Box>
