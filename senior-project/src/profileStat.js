a264aab44eb78767ee47ba66da740ea9aecb2b4e
Anna-Mclenko <amikhailenko@ewu.edu>
2024-01-17 20:57:25 -0800

    added a toggle between profile views

---
+++ senior-project/src/profileStat.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Grid, Box, Typography} from "@mui/material";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CheckIcon from '@mui/icons-material/Check';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+
+function ProfileStat ({data}) {
+    return(
+        <>
+        <Box container direction={"column"} sx={{width: "70%", marginLeft: "5%"}}> 
+                    <Typography variant="h3">{data.userName}</Typography>
+                    <Typography variant="caption">Joined: {data.joined}</Typography>
+                    <Grid container direction={"column"} sx={{marginTop: "5%"}}>
+                        <Typography variant="h4">Overall Statistics</Typography>
+                        <Grid container direction={"row"}>
+                            <LocationOnIcon/>
+                            <Typography>Number of Locations: {data.numloc}</Typography>
+                        </Grid>
+                        <Grid container direction={"row"}>
+                            <AssignmentIcon/>
+                            <Typography>Total Tasks: {data.numTasks}</Typography>
+                        </Grid>
+                        <Grid container direction={"row"}>
+                            <CheckIcon/>
+                            <Typography>Done Tasks: {data.doneTasks}</Typography>
+                        </Grid>
+                    </Grid> 
+                </Box>
+        </>
+    );
+}
+
+export default ProfileStat;
