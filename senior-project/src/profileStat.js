b2bfa60baece5b7d82e464a701edbbf29911aba0
Anna-Mclenko <amikhailenko@ewu.edu>
2024-01-19 22:21:27 -0800

    added visuals into settings view of profile page

--- senior-project/src/profileStat.js
+++ senior-project/src/profileStat.js
@@ -10,13 +10,13 @@ function ProfileStat ({data}) {
         <Box container="true" direction={"column"} sx={{width: "70%", marginLeft: "5%"}}> 
             <Typography variant="h3">{data.userName}</Typography>
             <Typography variant="caption">Joined: {data.joined}</Typography>
-            <Grid container direction={"column"} sx={{marginTop: "5%"}}>
+            <Grid container direction={"column"} rowGap={2} sx={{marginTop: "5%"}}>
                 <Typography variant="h4">Overall Statistics</Typography>
                 <Grid container direction={"row"}>
                     <LocationOnIcon/>
                     <Typography>Number of Locations: {data.numloc}</Typography>
                 </Grid>
-                <Grid containerdirection={"row"}>
+                <Grid container direction={"row"}>
                     <AssignmentIcon/>
                     <Typography>Total Tasks: {data.numTasks}</Typography>
                 </Grid>
--- senior-project/src/settings.js
+++ senior-project/src/settings.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Box, Typography} from "@mui/material";
+import { Grid, Box, Typography, Switch} from "@mui/material";
 
 function Settings({data}){
     return(
@@ -7,6 +7,38 @@ function Settings({data}){
         <Box container="true" direction={"column"} sx={{width: "70%", marginLeft: "5%"}}> 
             <Typography variant="h3">{data.userName}</Typography>
             <Typography variant="caption">Joined: {data.joined}</Typography>
+            <Grid container direction={"column"} rowGap={2} sx={{marginTop: "5%"}}> 
+                <Grid container direction={"row"} columnGap={2}>
+                    <Typography>Allow Access to My Location</Typography> 
+                    <Typography>Off</Typography>
+                    <Switch color="secondary" defaultChecked></Switch>
+                    <Typography>On</Typography>
+                </Grid>
+                <Grid container direction={"row"} columnGap={2}>
+                    <Typography>Notifications</Typography>
+                    <Typography>Off</Typography>
+                    <Switch color="secondary" defaultChecked></Switch>
+                    <Typography>On</Typography>
+                </Grid>
+                <Grid container direction={"row"} columnGap={2}>
+                    <Typography>Snoozing Notifications</Typography>
+                    <Typography>Off</Typography>
+                    <Switch color="secondary" defaultChecked></Switch>
+                    <Typography>On</Typography>
+                </Grid>
+                <Grid container direction={"row"} columnGap={2}>
+                    <Typography>Friend Sharing</Typography>
+                    <Typography>Off</Typography>
+                    <Switch color="secondary" defaultChecked></Switch>
+                    <Typography>On</Typography>
+                </Grid>
+                <Grid container direction={"row"} columnGap={2}>
+                    <Typography>Repeating Tasks</Typography>
+                    <Typography>Off</Typography>
+                    <Switch color="secondary" defaultChecked></Switch>
+                    <Typography>On</Typography>
+                </Grid>
+            </Grid>
         </Box>
         </>
     )
