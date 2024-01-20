7cee8756519ca3c4b48a2564032083796a8deb73
Anna-Mclenko <amikhailenko@ewu.edu>
2024-01-19 21:31:50 -0800

    added more visuals to the profile page

--- senior-project/src/profileAccount.js
+++ senior-project/src/profileAccount.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Grid, Box, Typography} from "@mui/material";
+import { Grid, Box, Typography, Button} from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function ProfileAccount({data}){
     return(
@@ -7,6 +8,12 @@ function ProfileAccount({data}){
         <Box container="true" direction={"column"} sx={{width: "70%", marginLeft: "5%"}}> 
             <Typography variant="h3">{data.userName}</Typography>
             <Typography variant="caption">Joined: {data.joined}</Typography>
+            <Grid container direction={"column"} rowGap={2} sx={{marginTop: "5%"}}> 
+                <Typography>Email: </Typography>
+                <Button sx={{maxWidth: "20%"}} variant="contained">Change Email</Button>
+                <Button sx={{maxWidth: "20%"}} variant="contained">Change Password</Button>
+                <Button sx={{maxWidth: "30%"}} variant="contained" startIcon={<AccountCircleIcon/>}>Change Profile Picture</Button>
+            </Grid>
         </Box>
         </>
     )
