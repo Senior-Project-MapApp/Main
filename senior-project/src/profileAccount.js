afa4e9e09fae7e2bd8c9e8ffb1b32df3c3a61d69
Anna-Mclenko <amikhailenko@ewu.edu>
2024-01-23 22:29:43 -0800

    refactor Account in profile

--- senior-project/src/profileAccount.js
+++ senior-project/src/profileAccount.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Box, Typography, Button} from "@mui/material";
+import { Grid, Box, Typography, Button, TextField} from "@mui/material";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function ProfileAccount({data}){
@@ -8,11 +8,18 @@ function ProfileAccount({data}){
         <Box container="true" direction={"column"} sx={{width: "70%", marginLeft: "5%"}}> 
             <Typography variant="h3">{data.userName}</Typography>
             <Typography variant="caption">Joined: {data.joined}</Typography>
-            <Grid container direction={"column"} rowGap={2} sx={{marginTop: "5%"}}> 
-                <Typography>Email: </Typography>
-                <Button sx={{maxWidth: "20%"}} variant="contained">Change Email</Button>
-                <Button sx={{maxWidth: "20%"}} variant="contained">Change Password</Button>
-                <Button sx={{maxWidth: "30%"}} variant="contained" startIcon={<AccountCircleIcon/>}>Change Profile Picture</Button>
+            <Typography variant="h5" sx={{marginTop: "5%"}}>Account Information:</Typography>
+            <Grid container direction={"row"} columnGap={2} sx={{marginTop: "2%"}}> 
+                <TextField label="Name" defaultValue={data.userName} sx={{width: "25ch"}}/>
+                <TextField label="Email" defaultValue={"data.email"} sx={{width: "25ch"}}/>
+            </Grid>
+            <Grid container direction={"row"} columnGap={2} sx={{marginTop: "2%"}}> 
+                <TextField label="Password" defaultValue={"data.password?"} sx={{width: "25ch"}}/>
+                <TextField label="Birthday" defaultValue={"data.birthday?"} sx={{width: "25ch"}}/>
+            </Grid>
+            <Grid container direction={"row"} columnGap={2} sx={{marginTop: "2%"}}> 
+                <Button variant="contained">Change Password</Button>
+                <Button variant="contained">Change Avatar</Button>
             </Grid>
         </Box>
         </>
