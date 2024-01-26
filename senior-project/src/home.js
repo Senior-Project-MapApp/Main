9ca816bf16c5d571db0d6ed8c4d2fbdc161a7430
Anna-Mclenko <amikhailenko@ewu.edu>
2024-01-25 21:48:10 -0800

    added new task button into home page

--- senior-project/src/home.js
+++ senior-project/src/home.js
@@ -1,10 +1,18 @@
+import React from "react";
 import TableGraph from "./tableGraph";
+import {Box, Grid, Button } from "@mui/material";
+import AddTaskIcon from '@mui/icons-material/AddTask';
 
 function Home ({data}) {
     return(
-        <>
-            <TableGraph data={data}/>
-        </>
+        <Grid container direction={"row"}>
+            <Box container sx={{width: "85%", margin: "auto"}}>
+                <TableGraph data={data}/>
+            </Box>
+            <Box container sx={{width: "11%", marginTop: "2%",}}>
+                    <Button variant="contained" endIcon={<AddTaskIcon/>}>New Task</Button>
+            </Box>
+        </Grid>
     );
 }
 
