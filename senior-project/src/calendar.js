262247c7b334fdded1e8cae03d7bea84d6692362
Anna-Mclenko <amikhailenko@ewu.edu>
2024-01-25 21:25:46 -0800

    added new task button to calendar

--- senior-project/src/calendar.js
+++ senior-project/src/calendar.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Grid, Box } from "@mui/material";
+import { Grid, Box, Button } from "@mui/material";
 import TableGraph from "./tableGraph";
+import AddTaskIcon from '@mui/icons-material/AddTask';
 
 function Calendar ({data}) {
     return (
         <>
         <Grid container direction={"row"}>
             <Box sx={{width: "60%"}}>
-                
+                <Button sx={{marginTop: "65%", marginLeft: "80%"}} variant="contained" endIcon={<AddTaskIcon/>}>New Task</Button>
             </Box>
             <Box sx={{width: "40%"}}>
                 <TableGraph data={data}/>
