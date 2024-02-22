a574f5e0f7e6f95af967cc90f5491b689f8d439e
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-21 17:50:53 -0800

    added a date range functionality for creating a new task

--- senior-project/src/createNewTask.js
+++ senior-project/src/createNewTask.js
@@ -7,37 +7,17 @@ import { ref, set } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-dat
 function NewTaskModal({open, handleClose, db, user}){
 
     const [date, setDate] = useState(new Date());
+    const [startD, setStartD] = useState();
+    const [endD, setEndD] = useState();
 
-    return(
-        <Dialog fullWidth onClose={handleClose} open={open}>
-            <Grid container sx={{marginBottom: "5%"}} direction={"column"} justifyContent={"center"} alignItems={"center"} rowGap={8}>
-                <DialogTitle color={"secondary"} variant="h4">Create New Task</DialogTitle>
-                <Grid container direction={"row"} justifyContent={"center"} alignItems={"center"} columnGap={4}>
-                    <Typography>Date to finish Task By:</Typography>
-                    <DatePicker selected={date} onChange={(date) => setDate(date)}/>
-                </Grid>
-                <Grid container direction={"row"} justifyContent={"center"} alignItems={"center"} columnGap={4}>
-                    <Typography>Task Name:</Typography>
-                    <TextField color="secondary" variant="outlined" label="Task Name"/>
-                </Grid>
-                <Grid container direction={"row"} justifyContent={"center"} alignItems={"center"} columnGap={4}>
-                    <Typography>Task Description:</Typography>
-                    <TextField multiline color="secondary" variant="outlined" label="Description"/>
-                </Grid>
-                <Grid container direction={"row"} justifyContent={"center"} alignItems={"center"} columnGap={4}>
-                    <Typography>Task Location:</Typography>
-                    <TextField color="secondary" variant="outlined" label="Location"/>
-                </Grid>
-                <Button variant="contained" type="submit" onClick={handleClose}>Create</Button>
-            </Grid>
-        </Dialog>
-    );
-}
+    const handleChange = (range) => {
+        const [startD, endD] = range;
+        setStartD(startD);
+        setEndD(endD);
+    };
 
-export default NewTaskModal;
-
-const createTaskBtn = document.getElementById('createTaskBtn');
-createTaskBtn.addEventListener("click", () => {
+   /* const createTaskBtn = document.getElementById('createTaskBtn');
+    createTaskBtn.addEventListener("click", () => {
     const title = document.getElementById('setTitle').value;
     const description = document.getElementById('setDescription').value;
     const date = document.getElementById('setDate').value;
@@ -59,4 +39,32 @@ createTaskBtn.addEventListener("click", () => {
     } else {
         alert("You must be logged in and input a title to create a task!")
     }
-});
+    });*/
+
+    return(
+        <Dialog fullWidth onClose={handleClose} open={open}>
+            <Grid container sx={{marginBottom: "5%"}} direction={"column"} justifyContent={"center"} alignItems={"center"} rowGap={8}>
+                <DialogTitle color={"secondary"} variant="h4">Create New Task</DialogTitle>
+                <Grid container direction={"row"} justifyContent={"center"} alignItems={"center"} columnGap={4}>
+                    <Typography>Date Range of Task:</Typography>
+                    <DatePicker selected={startD} onChange={handleChange} startDate={startD} endDate={endD} selectsRange/>
+                </Grid>
+                <Grid container direction={"row"} justifyContent={"center"} alignItems={"center"} columnGap={4}>
+                    <Typography>Task Name:</Typography>
+                    <TextField color="secondary" variant="outlined" label="Task Name"/>
+                </Grid>
+                <Grid container direction={"row"} justifyContent={"center"} alignItems={"center"} columnGap={4}>
+                    <Typography>Task Description:</Typography>
+                    <TextField multiline color="secondary" variant="outlined" label="Description"/>
+                </Grid>
+                <Grid container direction={"row"} justifyContent={"center"} alignItems={"center"} columnGap={4}>
+                    <Typography>Task Location:</Typography>
+                    <TextField color="secondary" variant="outlined" label="Location"/>
+                </Grid>
+                <Button variant="contained" type="submit" onClick={handleClose}>Create</Button>
+            </Grid>
+        </Dialog>
+    );
+}
+
+export default NewTaskModal;
