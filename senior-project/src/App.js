d0886a04e318f617b4c74bc4adcdf4fbcdfd311d
cnphillips3 <cphillips12@ewu.edu>
2024-02-28 15:49:23 -0800

    fixed user object passing

--- senior-project/src/App.js
+++ senior-project/src/App.js
@@ -80,9 +80,9 @@ function App() {
       { sign ? <Navbar user={currUser} HandleSignOut={HandleSignOut}/> : <></>}
       <Routes>
         <Route path="/SignIn" element={<SignIn/>}/>
-        <Route path="/home" element={<Home data={data} sign={sign} db={db} user={user}/>}/>
-        <Route path="/mapview" element={<Map data={data} sign={sign} db={db} user={user}/>}/>
-        <Route path="calendarview" element={<Calendar data={data} sign={sign} db={db} user={user}/>}/>
+        <Route path="/home" element={<Home data={data} sign={sign} db={db} user={currUser}/>}/>
+        <Route path="/mapview" element={<Map data={data} sign={sign} db={db} user={currUser}/>}/>
+        <Route path="calendarview" element={<Calendar data={data} sign={sign} db={db} user={currUser}/>}/>
         <Route path="profile" element={<Profile user={currUser} data={data} sign={sign}/>}/>
       </Routes>
 
