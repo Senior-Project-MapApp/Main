efe6ba23686420463fd28271bf74eaef3bcd8870
Anna-Mclenko <amikhailenko@ewu.edu>
2024-03-06 15:23:56 -0800

    mode the get task from task file to app file

--- senior-project/src/App.js
+++ senior-project/src/App.js
@@ -75,6 +75,32 @@ function App() {
   function HandleSignOut() {
     signOut(auth);
   }
+
+    let description, endDate, startDate, loc, tasks, i;
+    const getTask = () => {
+        const reference = ref(db, 'users/' + user.uid + '/test'/* + title*/);
+        onValue(reference, (snapshot) => {
+            description = snapshot.val().desc;
+            endDate = snapshot.val().endDate;
+            startDate = snapshot.val().startDate;
+            loc = snapshot.val().loc;
+        }, {
+            onlyOnce: true
+        });
+    }
+
+    const getAllTasks = () => {
+        const reference = ref(db, 'users/' + user.uid);
+        onValue(reference, (snapshot) => {
+            tasks = snapshot.val();
+        }, {
+            onlyOnce: true
+        });
+        for (i = 0; i < tasks.length; i++) {
+            console.log('Title: ' + tasks[i]);
+        }
+    }
+  
   return (
     //This is where the sign in page will be
     //Once the user signs in, they will be sent to the home page
