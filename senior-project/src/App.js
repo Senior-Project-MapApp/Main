461570d4bcb282d713baf38ad0140d057e31ed67
ryan-mcvay73 <146029286+ryan-mcvay73@users.noreply.github.com>
2024-03-08 22:09:26 -0800

    Added parameters in Calendar Route

--- senior-project/src/App.js
+++ senior-project/src/App.js
@@ -119,7 +119,7 @@ function App() {
         <Route path="/SignIn" element={<SignIn/>}/>
         <Route path="/home" element={<Home data={data} sign={sign} db={db} user={currUser} getTask={getTask} getAllTasks={getAllTasks}/>}/>
         <Route path="/mapview" element={<Map data={data} sign={sign} db={db} user={currUser}/>}/>
-        <Route path="calendarview" element={<Calendar data={data} sign={sign} db={db} user={currUser}/>}/>
+        <Route path="calendarview" element={<Calendar data={data} sign={sign} db={db} user={currUser} getTask={getTask} getAllTasks={getAllTasks}/>}/> 
         <Route path="profile" element={<Profile user={currUser} data={data} sign={sign}/>}/>
       </Routes>
 
